443e6fd1f9f2fd7a03a01b9999482cab4251087a
marshall <obasimarshall@outlook.com>
2025-12-09 17:36:39 -0600

    updated allowedOrigins.js to accept requests from only frontend and payment server

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -1,11 +1,8 @@
+const CONFIG = require("./config");
+
 const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:5000',
-  'http://127.0.0.1:3000',
-  'http://127.0.0.1:5000',
-  'http://localhost:8080',
-  'http://192.168.1.182:3000',
-  'https://52db0eb38991.ngrok-free.app'
+  CONFIG.NATIONAL_WEBHOOK_URL,
+  CONFIG.FRONTEND_URL
 ];
 
 module.exports = allowedOrigins;
