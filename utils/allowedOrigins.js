c742e9a35f506af21b8f4fde01545979da0337c8
marshall <obasimarshall@outlook.com>
2025-12-09 18:52:12 -0600

    updated enable database sync

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -3,8 +3,6 @@ const CONFIG = require("./config");
 const npb = CONFIG.NATIONAL_WEBHOOK_URL;
 const fu = CONFIG.FRONTEND_URL;
 
-console.log("ORIGINS", npb, fu);
-
 const allowedOrigins = [
   fu,
   npb
